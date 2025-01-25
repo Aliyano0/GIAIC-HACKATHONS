0821f70891ec63841eda583af86f6854bc52adcc
Aliyano0 <aliyanaqeel97@gmail.com>
2025-01-25 07:19:01 +0500

    Day-6 Fixed some deployment errors.

--- Marketplace-Hackathon/marketplace-hackathon/src/sanity/schemaTypes/index.ts
+++ Marketplace-Hackathon/marketplace-hackathon/src/sanity/schemaTypes/index.ts
@@ -2,11 +2,10 @@ import { type SchemaTypeDefinition } from 'sanity'
 import product from './product'
 import customers from './customers'
 import order from './order'
-import deliveryZone from './deliveryZone'
 import reviews from './reviews'
 import shipment from './shipment'
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product, customers, order, deliveryZone, reviews, shipment],
+  types: [product, customers, order, reviews, shipment],
 }
