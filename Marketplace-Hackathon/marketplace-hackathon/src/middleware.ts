e7d3e55b5bf4c20b046058a43ea487eb7830a15c
Aliyano0 <aliyanaqeel97@gmail.com>
2025-02-06 04:16:55 +0500

    Fixed clerk middleware error

--- Marketplace-Hackathon/marketplace-hackathon/src/middleware.ts
+++ Marketplace-Hackathon/marketplace-hackathon/src/middleware.ts
@@ -1,18 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
-
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
+const myMiddleware = async (req) => {
+  try {
+    return clerkMiddleware(req)
+  } catch (error) {
+    console.error('Clerk Middleware Error:', error)
+    return new Response('Internal Server Error', { status: 500 })
   }
-})
+}
 
+export default myMiddleware
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 }
