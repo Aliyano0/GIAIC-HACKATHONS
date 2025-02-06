efadb59fbdf902a9d3216070aedac896a9eba86c
Aliyano0 <aliyanaqeel97@gmail.com>
2025-02-07 03:04:02 +0500

    Fixed image overflowing.

--- Marketplace-Hackathon/marketplace-hackathon/src/app/(store)/components/HomepageComponents/BrandInfo.tsx
+++ Marketplace-Hackathon/marketplace-hackathon/src/app/(store)/components/HomepageComponents/BrandInfo.tsx
@@ -7,7 +7,7 @@ const BrandInfo = ({ heading, paragraph }: IBrandInfo) => {
   return (
     <div>
       <div className="intl-brands lg:w-auto">
-        <h5 className="text-2xl leading-[32.4px] md:text-[27px] lg:text-[40px] font-bold lg:leading-[54px]">
+        <h5 className="text-2xl leading-[32.4px] md:text-[27px] lg:text-[30px] xl:text-[40px] font-bold lg:leading-[54px]">
           {heading}
         </h5>
         <p className="text-xs leading-[22px] lg:text-base text-[#00000099]">
