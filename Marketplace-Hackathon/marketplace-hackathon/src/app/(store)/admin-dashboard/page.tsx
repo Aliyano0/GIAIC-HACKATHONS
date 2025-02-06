69c5651d6e2955bd5343dafb8612cabd351bc7ea
Aliyano0 <aliyanaqeel97@gmail.com>
2025-02-06 05:47:33 +0500

    Fixed clerk middleware error

--- Marketplace-Hackathon/marketplace-hackathon/src/app/(store)/admin-dashboard/page.tsx
+++ Marketplace-Hackathon/marketplace-hackathon/src/app/(store)/admin-dashboard/page.tsx
@@ -4,10 +4,10 @@
 async function AdminDashboard() {
   // checkRole is a util function that checks the user's role. If the user is not an admin, it will return false.
   // Protect the page from users who are not admins
-  // const isAdmin = await checkRole("admin");
-  // if (!isAdmin) {
-  //   redirect("/not-authorised");
-  // }
+  const isAdmin = await checkRole("admin");
+  if (!isAdmin) {
+    redirect("/not-authorised");
+  }
   return (
     <div>
       <h1>Admin Dashboard</h1>
